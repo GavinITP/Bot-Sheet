b77477903454386ef668aca3b43682383688ebf9
Teerpong <teerpong.me@gmail.com>
2024-09-09 21:41:02 +0700

    change from google.json to .env

--- src/helpers/getSheetData.js
+++ src/helpers/getSheetData.js
@@ -5,9 +5,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve(__dirname, "../../google.json"),
+  credentials: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle newlines in private key
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+  },
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
+
 const sheets = google.sheets({ version: "v4", auth });
 const spreadsheetId = process.env.GOOGLE_SHEET_ID;
 const botFrequency = process.env.BOT_FREQUENCY;
