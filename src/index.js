0e265dec95d2448e471ad5bea54493f2aee227b2
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-09-17 22:50:26 +0700

    feat: Add more console.log

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ async function run() {
   console.log("Start Bot");
 
   const browser = await puppeteer.launch({
-    headless: "new",
+    // headless: "new",
+    headless: false,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
@@ -48,15 +49,15 @@ async function run() {
     const PASSWORD = process.env.PASSWORD || "";
 
     if (!EMAIL || !PASSWORD) {
-      throw new Error("Please provide correct email and password");
+      throw new Error("Please provide correct email and password.");
     }
 
-    console.log("logging in...");
+    console.log("Logging In...");
 
     await page.setViewport({ width: 1200, height: 800 });
     await login(page, EMAIL, PASSWORD);
 
-    console.log("fetching sheet data...");
+    console.log("Fetching Google Sheet Data...");
 
     const allPostsData = await getSheetData();
 
@@ -64,24 +65,28 @@ async function run() {
       const { postUrl, groupNames } = postData;
 
       for (const groupName of groupNames) {
-        console.log("Going to url", postUrl);
+        console.log("Navigating to URL:", postUrl);
         await page.goto(postUrl);
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
-        console.log("Sharing...");
+        console.log("Clicking Share Button...");
         await clickShareButton(page);
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
+        console.log("Clicking Group Button...");
         await clickGroupButton(page);
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
+        console.log("Selecting Group...");
         await selectGroup(page, groupName);
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
+        console.log("Sharing");
         await clickPostButton(page);
         await new Promise((resolve) => setTimeout(resolve, 8000));
 
-        console.log(`${postUrl} Posted to ${groupName}\n`);
+        console.log(`Successfully share to ${groupName}\n`);
+        console.log("====================\n");
       }
     }
   } catch (err) {
