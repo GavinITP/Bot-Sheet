6012c521c3dc5d4ded1e8bcfd913bcb151f0f7bc
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-09-29 22:39:26 +0700

    refactor: change env example

--- src/index.js
+++ src/index.js
@@ -51,23 +51,23 @@ async function run() {
       for (const groupName of groupNames) {
         console.log("Navigating to URL:", postUrl);
         await page.goto(postUrl);
-        await new Promise((resolve) => setTimeout(resolve, 8000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         console.log("Clicking Share Button...");
         await clickShareButton(page);
-        await new Promise((resolve) => setTimeout(resolve, 8000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         console.log("Clicking Group Button...");
         await clickGroupButton(page);
-        await new Promise((resolve) => setTimeout(resolve, 8000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         console.log("Selecting Group...");
         await selectGroup(page, groupName);
-        await new Promise((resolve) => setTimeout(resolve, 8000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         console.log("Sharing");
         await clickPostButton(page);
-        await new Promise((resolve) => setTimeout(resolve, 8000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         console.log(`Successfully share to ${groupName}\n`);
         console.log("====================\n");
